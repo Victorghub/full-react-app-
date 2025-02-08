94d8f954cbc0303e856bac1a50eb1dd41600c80a
Victor <onetworacks@gmail.com>
2025-02-08 22:24:18 +0000

    fixed error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <div className="App">
-   <Nav /> ......
+   <Nav /> This finally works
     </div>
   );
 }
