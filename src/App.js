7cd69ae44126f23b34631e385c7a28af72e38a72
Victor <onetworacks@gmail.com>
2025-02-09 02:36:06 +0000

    landing page finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from './Components/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Landing from './Components/Landing';
 
 
 function App() {
   return (
     <div className="App">
    <Nav />
+   <Landing />
     </div>
   );
 }
