5a40a3b20098531c28bea65cdcee2ee7a3e70b41
Victor <onetworacks@gmail.com>
2025-02-15 14:51:45 +0000

    wont show up when making page dynamic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Landing from './Components/Landing';
 import Highlights from './Components/Highlights';
+import Featured from './Components/Featured';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
    <Nav />
    <Landing />
    <Highlights />
+   <Featured />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes, faBookOpen, faTags, faBolt } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBookOpen, faTags, faBolt, faStar, faStarHalfAlt, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
 
 
-library.add(faBars, faShoppingCart, faTimes, faBookOpen, faTags, faBolt)
+library.add(faBars, faShoppingCart, faTimes, faBookOpen, faTags, faBolt, faStar, faStarHalfAlt)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
